0f9288a965d56c3879759b1c2d446c0764fafaca
GabrielMoraes <gugapoeta@gmail.com>
2023-12-16 16:58:34 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const express = require("express");
-
 const app = require('./app');
+
+// const express = require("express");
+
 
 const port = process.env.PORT || 5000;
 
-
-app.use(express.json());
-app.use(require("./routes/record"));
+// app.use(express.json());
+// app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("../db/conn");
 
 app.listen(port, async () => {
+  const dbo = require("../db/conn");
+
   // perform a database connection when server starts
   await dbo.connectToServer(function (err) {
     if (err) console.error(err);
